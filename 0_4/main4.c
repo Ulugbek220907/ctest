f2ba14ba39159436e569419f989742b7570b5140
Ulugbek <190009699+Ulugbek220907@users.noreply.github.com>
2026-06-07 22:15:28 +0500

    Even number finder

---
+++ 0_4/main4.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+
+int main(){
+    int n = 0;
+
+    printf("Enter a number: ");
+    scanf("%d", &n);
+    printf("Even numbers:");
+    for (int i = 1; i < n; i++){
+        if (i % 2 == 0){
+            printf(" %d,", i);
+        }
+    }
+
+
+    return 0;
+}
