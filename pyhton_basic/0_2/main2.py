d320e3810c80011a14191fafc3b0bc3d68990114
Ulugbek <190009699+Ulugbek220907@users.noreply.github.com>
2026-06-28 23:31:37 +0500

    variables

---
+++ pyhton_basic/0_2/main2.py
@@ -0,0 +1,35 @@
+#variables 
+#int 
+#string
+#float
+#boolean
+
+
+#strings
+name = "John"
+surname = "Doe"
+
+#integer
+age = 30
+quantity = 10
+
+#float
+height = 5.9
+pi = 3.14
+
+#boolean
+is_student = True
+is_adult = True
+
+
+
+print("Name:", name)
+print("Surname:", surname)
+print("Age:", age)
+print("Quantity:", quantity)
+print("Height:", height)
+print("Pi:", pi)
+print("Is Student:", is_student)
+print("Is Adult:", is_adult)
+
+print(f"it is me {name} {surname} and I am {age} years old.")
