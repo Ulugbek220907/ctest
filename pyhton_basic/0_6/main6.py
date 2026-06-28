1890d5ed25b8641fa99816bd94b8df3e8157c797
Ulugbek <190009699+Ulugbek220907@users.noreply.github.com>
2026-06-29 00:20:30 +0500

    statements if else

---
+++ pyhton_basic/0_6/main6.py
@@ -0,0 +1,18 @@
+#if elif else statements
+
+a = int(input("Enter a number: "))
+
+if a > 10:
+    print("a is greater than 10")
+elif a == 10:
+    print("a is equal to 10")
+else:
+    print("a is less than 10")
+
+
+b = str(input("Enter a string: "))
+
+if b == "hello":
+    print("You entered 'hello'")
+else:
+    print("You did not enter 'hello'")
