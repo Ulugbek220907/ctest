be68c1bcc2fbcfb255a7061beafd448a4f01c0ef
Ulugbek <190009699+Ulugbek220907@users.noreply.github.com>
2026-06-29 00:27:49 +0500

    logical operators

--- pyhton_basic/0_6/main6.py
+++ pyhton_basic/0_6/main6.py
@@ -1,4 +1,5 @@
 #if elif else statements
+#logical operators: and, or, not
 
 a = int(input("Enter a number: "))
 
@@ -16,3 +17,14 @@ if b == "hello":
     print("You entered 'hello'")
 else:
     print("You did not enter 'hello'")
+
+
+
+c = int(input("Enter another number: "))
+
+if c > 0 and c < 10:
+    print("c is between 0 and 10")
+elif c < 0 or c > 10:
+    print("c is less than 0 or greater than 10")
+else:
+    print("c is equal to 0 or 10")
