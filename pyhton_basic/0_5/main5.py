a19dc788e299d3b4ca1bb3c58655e59f2acfdcec
Ulugbek <190009699+Ulugbek220907@users.noreply.github.com>
2026-06-29 00:10:22 +0500

    arithmetic operators

---
+++ pyhton_basic/0_5/main5.py
@@ -0,0 +1,32 @@
+friends = 0
+import math
+
+#arithmetic operators and math library
+
+friends += 1 #addition
+friends += 1
+friends -= 1 #subtraction
+friends *= 4 #multiplication
+friends /= 2 #division
+friends **= 2 #exponentiation 2** = 4 darajasi
+friends %= 3 #modulo 10%3 = 1 butun qismi
+friends //= 2 #floor division 10//3 = 3 
+
+
+x = 4.14
+y = -5
+z = 4
+
+#result = round(x) #4
+#result = abs(y) #5
+#result = max(x, y, z) #4
+#result = min(x, y, z) #4
+#result = pow(x, z) #4.14**4 = 295.499
+
+
+print(math.pi)
+print(math.e)
+print(math.sqrt(16)) #4
+print(math.ceil(4.2)) #5
+print(math.floor(4.9)) #4
+
