118cd2237d6355bfacf5869978086a0a9cf02e47
Ulugbek <190009699+Ulugbek220907@users.noreply.github.com>
2026-06-30 08:01:05 +0500

    while loop

---
+++ pyhton_basic/1_0/main10.py
@@ -0,0 +1,9 @@
+#while loop = execute code while some condition true
+
+name = input("Enter your name: ")
+
+
+while name != "ulugbek":
+    print("Please type your name.")
+    name = input("Enter your name: ")
+print("Your name is " + name + "! Thank you!")
