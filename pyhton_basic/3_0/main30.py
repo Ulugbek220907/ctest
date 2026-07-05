1e269752e37583f8db88eb0636e5f00621cfecda
Ulugbek <190009699+Ulugbek220907@users.noreply.github.com>
2026-07-05 22:36:12 +0500

    linked list

---
+++ pyhton_basic/3_0/main30.py
@@ -0,0 +1,82 @@
+#linked list implementation in python
+class Node:
+    def __init__ (self, data):
+        self.data = data
+        self.next = None
+
+
+class LinkedList:
+    #head and data -> next and data -> next and data
+    def __init__ (self):
+        self.head = None#list starts empty
+    
+    def append(self, data):
+        #new node creation
+        new_node = Node(data)
+
+        #if list is empty
+        if self.head is None:
+            self.head = new_node
+            return
+
+        #travel to the end of the list
+        current = self.head
+        while current.next:
+            current = current.next
+        
+        #and append the new node
+        current.next = new_node
+    
+    def prepend(self, data):
+        #new node created
+        new_node = Node(data)
+
+        #new first node will hold ponter of the old head
+        new_node.next = self.head
+        #linked list's head will be head
+        self.head = new_node
+        #new node's given data
+        new_node.data = data
+
+    def leng(self):
+        current = self.head
+        a = 0 #reason for this is we just declare empty node so it will be first node, num of nodes = 1
+        while current: #current is used for traveling to the end if the list complexity is O(n)
+            a += 1
+            current = current.next
+        print(a)
+
+    def delete(self, value):
+        current = self.head
+
+        while current.next: #loop will run until the last node
+            if current.next.data == value: #check
+                #current.next will point to the next node of the node to be deleted
+
+                #assining the next of the current node to the next of the node to be deleted
+                current.next = current.next.next 
+                #returning from the function after deleting the node
+                return
+            
+            current = current.next #jumps to the next node
+
+    def display(self):
+        current = self.head
+        while current:
+            print(current.data, end=" -> ")
+            current = current.next
+        print("None")
+    
+
+
+my_list = LinkedList()
+my_list.append(10)
+my_list.append(20)
+my_list.prepend(34)
+my_list.append(50)
+my_list.prepend(23)
+my_list.leng()
+#23 -> 34 -> 10 -> 20 -> 50 -> None
+my_list.delete(10)
+my_list.display()
+# 23 -> 34 -> 20 -> 50 -> None
