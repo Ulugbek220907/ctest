5261b54f5df15282fecf66817885ba1cc4303cc5
Ulugbek <190009699+Ulugbek220907@users.noreply.github.com>
2026-07-06 09:28:22 +0500

    mergetwo list

---
+++ python_algo_solutions/1_1/main11.py
@@ -0,0 +1,20 @@
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def mergetwolist(self, list1, list2):
+        if not list1:
+            return list2
+        if not list2:
+            return list1
+
+        if list1.val < list2.val:
+            list1.next = self.mergetwolist(list1.next, list2)
+            return list1
+        else:
+            list2.next = self.mergetwolist(list1, list2.next)
+            return list2
+
