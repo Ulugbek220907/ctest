4f9dddf5150a58f33ab4cd51ab013e7749e64ea1
Ulugbek <190009699+Ulugbek220907@users.noreply.github.com>
2026-07-08 01:03:43 +0500

    arithmetic operators

---
+++ java/0_3/main3.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class main3{
+    
+    public static void main(String[] args){
+        //Arithmetic operators
+
+        int x = 10;
+        int y = 2;
+        int z;
+
+        z = x + y; // addition 12
+        z = x - y; // subtraction 8
+        z = x * y; // multiplication 20
+        z = x / y; // division 5
+        z = x % y; // modulus if x=11 then z=1
+        
+        z = 10;
+        //increment & decrement
+        z++;
+        z--;
+        --z;
+        ++z;
+
+
+        System.out.print("=> " + z);
+    }
+}
