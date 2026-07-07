bace3cb75606fc47de66568e81839c7da0dd5e33
Ulugbek <190009699+Ulugbek220907@users.noreply.github.com>
2026-07-08 00:44:10 +0500

    input output

--- java/0_2/main2.java
+++ java/0_2/main2.java
@@ -2,16 +2,45 @@ import java.util.Scanner;
 
 public class main2 {
     public static void main(String[] args) {
+        //Area calculator
+        double height, width, area;
+
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter a number: ");
-        int number = scanner.nextInt();
-        
-        System.out.print("Enter your age: ");
-        int age = scanner.nextInt();
 
-        System.out.println("Your number is: " + number + "\nYour age is " + age);
-        
+        System.out.print("Enter width: ");
+        width = scanner.nextDouble();
 
+        System.out.print("Enter height: ");
+        height = scanner.nextDouble();
+
+        area = height*width;
+
+        System.out.print("Area is " + area + "\n");
+
+
+        //Volume calc
+
+        System.out.print("Enter width: ");
+        width = scanner.nextDouble();
+
+        System.out.print("Enter height: ");
+        height = scanner.nextDouble();
+
+        System.out.print("Enter length: ");
+        double length = scanner.nextDouble();
+
+        double volume = length*width*height;
+
+        System.out.print("Volume is " + volume + "\n");
+
+        // student check
+
+        System.out.print("Are you a student?: ");
+        boolean isstudent = scanner.nextBoolean();
+
+        System.out.print(isstudent);
+
+        
         scanner.close();
     }
 
