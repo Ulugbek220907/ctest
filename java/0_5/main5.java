1c464a02b1194276d00da958950fdbd9152981be
Ulugbek <190009699+Ulugbek220907@users.noreply.github.com>
2026-07-08 15:41:57 +0500

    if else

---
+++ java/0_5/main5.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+
+public class main5{
+
+
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+
+        int age = 10;
+        //if else statement
+
+        if (age < 18){
+            System.out.println("You are a minor.");
+        } else if (age >= 18 && age < 65){
+            System.out.println("You are an adult.");
+        } else {
+            System.out.println("You are a senior citizen.");
+        }
+    }
+}
