00c018901cae5c253fcda9ae49922912f090368a
Ulugbek <190009699+Ulugbek220907@users.noreply.github.com>
2026-07-13 23:33:27 +0500

    writing to file

---
+++ java/2_1/main21.java
@@ -0,0 +1,15 @@
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class main21{
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+
+        try(FileWriter writer = new FileWriter("output.txt")) {
+            writer.write("Hello, World!");
+            System.out.println("Successfully wrote to the file.");
+        } catch (IOException e) {
+            System.out.println("An error occurred");
+        }
+    }
+}
