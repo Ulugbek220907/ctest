7978f78548b99c67ad798b0d51b37750bf961f0d
Ulugbek <190009699+Ulugbek220907@users.noreply.github.com>
2026-07-28 23:46:35 +0500

    pandas

---
+++ python_advanced/libs/numpy/0_2/main2.py
@@ -0,0 +1,17 @@
+import pandas as pd
+
+
+data = {
+    'Name': ['Alice', 'Bob', 'Charlie', 'Diana'],
+    'Age': [28, 34, 22, 31],
+    'Department': ['Tech', 'Sales', 'Tech', 'HR'],
+    'Salary': [85000, 62000, 78000, 69000]
+}
+
+
+df = pd.DataFrame(data)
+
+
+#print(df['Salary'])
+#print(df['Salary'].mean())
+
